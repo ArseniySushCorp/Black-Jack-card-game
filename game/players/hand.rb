33eac6a2ad9e6f6c6b1bc684753a3a7d8fa2aa2f
Arseniy Suschevskiy <a.suschevskiy@voiso.com>
2021-05-13 18:48:41 +0300

    Delete scoring, add methods to hand class

--- game/players/hand.rb
+++ game/players/hand.rb
@@ -1,5 +1,6 @@
 class Hand
   include Scoring
+  include Variables
 
   attr_accessor :cards
 
@@ -21,5 +22,42 @@ class Hand
 
   def fold_cards
     @cards = []
+  end
+
+  private
+
+  def count_points(cards)
+    count = without_ace(cards) || 0
+
+    include_ace?(cards) ? count_ace(cards, count) : count
+  end
+
+  def without_ace(cards)
+    is_picture = ->(x) { PICTURE_CARDS.include? x }
+
+    cards.reduce(0) do |points, card|
+      case card[:value]
+
+      when '1'..'10'  then points + card[:value].to_i
+      when is_picture then points + 10
+      else points
+      end
+    end
+  end
+
+  def count_ace(cards, count)
+    cards.reduce(count) do |points, card|
+      if card.value? 'A'
+        biggest = points + 11
+
+        points += biggest > 21 ? 1 : 11
+      else
+        points
+      end
+    end
+  end
+
+  def include_ace?(cards)
+    cards.any? { |card| card.value? 'A' }
   end
 end
